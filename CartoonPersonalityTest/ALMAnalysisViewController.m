17c63d74a1e8f0f067c9d9617b692dbef7e17a97
AliMar11 <alicia.marisal@gmail.com>
2017-01-08 23:51:44 -0500

    added introduction animation for doppel runner ups and finalVC

--- CartoonPersonalityTest/ALMAnalysisViewController.m
+++ CartoonPersonalityTest/ALMAnalysisViewController.m
@@ -6,7 +6,6 @@
 //  Copyright © 2016 AliApps. All rights reserved.
 //
 
-//added name in textView and animated button for EndVC
 #import <QuartzCore/QuartzCore.h>
 #import "ALMAnalysisViewController.h"
 #import "ALMCharacter.h"
@@ -26,17 +25,17 @@
 //TODO: ANIMATE THIS PAGE
 @implementation ALMAnalysisViewController
 
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    NSLog(@"\n\n 1 RUNNERUPS SETUP METHOD CALLED \n\n");
 }
 
 -(void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear: animated];
     
-    NSLog(@" 2 VIEW WILL APPEAR");
+    NSLog(@" 1 VIEW WILL APPEAR");
     
     CAGradientLayer *background = [ALMBackgroundLayer blueGradient];
     background.frame = self.view.bounds;
@@ -45,34 +44,31 @@
     self.finallyButton.layer.cornerRadius = 6;
     self.finallyButton.titleLabel.lineBreakMode = NSLineBreakByWordWrapping;
     self.finallyButton.clipsToBounds = YES;
-    
+    self.finallyButton.hidden = YES;
+
     self.characterTwo.alpha = 0;
     self.characterThree.alpha = 0;
     self.characterFour.alpha = 0;
     self.characterFive.alpha = 0;
     
-    self.finallyButton.hidden = YES;
-    
     [self introduceRunnerUpsWithCompletion: nil];
-    
-    NSLog(@" 3 VIEW WILL APPEAR");
 }
 
 -(void)introduceRunnerUpsWithCompletion:(void(^)())completion
 {
-    NSLog(@"\n\n 4 TEXT INTRO\n\n");
-
+    NSLog(@"\n\n 2 TEXT INTRO\n\n");
+    
     NSString *topFourIntro = [NSString stringWithFormat: @"%@, answers have been analyzed... some honrable mentions are:", self.playerTraits.userName];
     
     UITextView *topFourView = [[UITextView alloc]init];
     [self.view insertSubview: topFourView atIndex: 0];
-
+    
     topFourView.hidden = YES;
     topFourView.text = topFourIntro;
     topFourView.backgroundColor = [UIColor clearColor];
     topFourView.layer.shadowColor = [UIColor clearColor].CGColor;
     [topFourView setFont: [UIFont fontWithName: @"Verdana-Bold" size: 15]];
-
+    
     CGFloat textViewX =  (self.view.frame.origin.x) + 30;
     CGFloat textViewY =  (self.view.frame.origin.y) + 25;
     CGFloat textViewWidth = CGRectGetWidth(self.view.bounds) / 1.1;
@@ -81,40 +77,36 @@
     CGRect textViewFrame = CGRectMake(textViewX, textViewY, textViewWidth, textViewheight);
     topFourView.frame = textViewFrame;
     
-    NSLog(@"\n\n 5 MESSAGE CONSTRAINTS SET, ABOUT TO CALL TRANSITION \n\n");
-
-                             [UIView transitionWithView: topFourView
-                                               duration: 3.5f
-                                                options: UIViewAnimationOptionCurveEaseInOut |
-                              UIViewAnimationOptionTransitionCrossDissolve
-                                             animations: ^{
-                                                 NSLog(@"\n\n 6 TRANSITION CALLED\n\n");
-
-                                                 topFourView.alpha = 1;
-                                                 topFourView.hidden = NO;
-                                                 topFourView.alpha = 0;
-                                             
-                                                 NSLog(@"\n\n 7 TRANSITION FINISHED\n\n");
-                                             }
-                                             completion:^(BOOL finished)
-                              
-                                            {
-                                              NSLog(@"\n\n 8 COMPLETION FOR MESSAGE \n\n");
-                                              [self presentRunnerUps];
-                                              
-                                              NSLog(@"\n\n 9STACK VIEW FOR RUNNERUP ABOUT TO BE CALLED\n\n");
-                                              
-                                      
-                                          }];
-
+    NSLog(@"\n\n 3 TEXTVIEW SET, ABOUT TO CALL TRANSITION \n\n");
     
-// NSString *topDoppelIntro = @"your cartoon doppel is...";
+    [UIView transitionWithView: topFourView
+                      duration: 3.5f
+                       options: UIViewAnimationOptionCurveEaseInOut |
+     UIViewAnimationOptionTransitionCrossDissolve
+                    animations: ^{
+                        NSLog(@"\n\n 4 TRANSITION CALLED\n\n");
+                        
+                        topFourView.alpha = 1;
+                        topFourView.hidden = NO;
+                        topFourView.alpha = 0;
+                        
+                        NSLog(@"\n\n 5 TRANSITION FINISHED\n\n");
+                    }
+                    completion:^(BOOL finished)
+     
+     {
+         [self presentRunnerUps];
+         
+         NSLog(@"\n\n 6 STACK VIEW FOR RUNNERUP CALLED\n\n");
+         
+     }];
+    
 }
 
 -(void)presentRunnerUps
 {
-    NSLog(@"\n\n 10 STACK VIEW SHOW METHOD CALLED\n\n");
-
+    NSLog(@"\n\n 7 STACK VIEW SHOW METHOD CALLED\n\n");
+    
     //TODO break up the sizable code below
     [ALMAnalysis sortUserTraits: self.playerTraits withCompletion:^(NSArray *sortedTraitsArray)
      {
@@ -127,11 +119,11 @@
                    self.doppelListWithTraits =  orderedDoppelTraits;
                    
 //Here we do the work of comparing player.traits with doppel.traits and setting the images of lower 4 of top 5 matches, then passing the #1 match to as 'topDog' to nextVC.
-                 
+                   
                    [ALMAnalysis dataAnalysis: self.topPlayerTraits : self.doppelListWithTraits withCompletion:^(NSArray *topFiveHanchos)
                     {
-                        NSLog(@"\n\n 11 STACK VIEW\n\n");
-
+                        NSLog(@"\n\n 12 STACK VIEW\n\n");
+                        
                         [self.characterTwo setImage: [topFiveHanchos[1]mugshot]];
                         self.characterTwo.layer.cornerRadius = 10;
                         self.characterTwo.clipsToBounds = YES;
@@ -151,39 +143,76 @@
                         [UIView transitionWithView: self.doppelStackView
                                           duration:1.5f
                                            options: UIViewAnimationOptionCurveLinear
-                         
-                                        animations:^{
-                                          
-                                            NSLog(@"\n\n 12 STACKVIEW ANIMATION START\n\n");
-
-                                            self.characterTwo.alpha = 1;
-                                            self.characterThree.alpha = 1;
-                                            self.characterFour.alpha = 1;
-                                            self.characterFive.alpha = 1;
-                                            
-                                            NSLog(@"\n\n 13 STACK VIEW ANIMATION END\n\n");
-                                        }
-                         
+                                        animations:
+                         ^{
+                            
+                            NSLog(@"\n\n 8 STACKVIEW ANIMATION START\n\n");
+                            
+                            self.characterTwo.alpha = 1;
+                            self.characterThree.alpha = 1;
+                            self.characterFour.alpha = 1;
+                            self.characterFive.alpha = 1;
+                         }
+         
                                         completion:^(BOOL finished)
                                          {
+                                             NSLog(@"\n\n 9 other TEXTVIEW COMPLETION CALLED\n\n");
                                              
-                                             NSLog(@"\n\n 14 STACKIEW COMPLETION CALLED\n\n");
-
                                              self.topMatch = topFiveHanchos[0];
                                              
-                                             [UIView animateWithDuration: 1 animations:
+                                             UITextView *presentNextButton = [[UITextView alloc] init];
+                                             NSString *buttonIntro = @"and your doppel is...";
+                                             presentNextButton.hidden = YES;
+                                             CGFloat textViewX =  (self.doppelStackView.frame.origin.x) + 30;
+                                             CGFloat textViewY =  (self.doppelStackView.frame.origin.y) + 350;
+                                             CGFloat textViewWidth = CGRectGetWidth(self.self.doppelStackView.bounds);
+                                             CGFloat textViewheight = CGRectGetHeight(self.doppelStackView.bounds) / 2;
+                                             
+                                             CGRect textViewFrame = CGRectMake(textViewX, textViewY, textViewWidth, textViewheight);
+                                             presentNextButton.frame = textViewFrame;
+                                             
+                                             presentNextButton.text = buttonIntro;
+                                             presentNextButton.backgroundColor = [UIColor clearColor];
+                                             presentNextButton.layer.shadowColor = [UIColor clearColor].CGColor;
+                                             [presentNextButton setFont: [UIFont fontWithName: @"Verdana-Bold" size: 15]];
+                                             
+                                             [UIView transitionWithView: presentNextButton
+                                                               duration: 3.0f
+                                                                options: UIViewAnimationOptionCurveEaseInOut |
+                                              UIViewAnimationOptionTransitionCrossDissolve
+                                                             animations:
                                               ^{
-                                                 self.finallyButton.hidden = NO;
+                                                 [self.doppelStackView insertSubview: presentNextButton atIndex: 0];
+                                                  
+                                                 NSLog(@"\n\n 10 BUTTON TEXTVIEW TRANSITION CALLED\n\n");
+                                                  [UIView transitionWithView: presentNextButton duration: 1 options:UIViewAnimationOptionTransitionCrossDissolve animations:
+                                                   ^{
+                                                       presentNextButton.alpha = 1;
+                                                       presentNextButton.hidden = NO;
+                                                       presentNextButton.alpha = 0;
+                                                   }
+                                                    completion: nil];
 
-                                             }];
+                                                  
+                                                  NSLog(@"\n\n 11 TRANSITION FINISHED\n\n");
 
+                                              }
+                                                    completion:^(BOOL finished)
+                                                    {
+                                                     [UIView animateWithDuration: 2 animations:
+                                                   ^{
+                                                      self.finallyButton.hidden = NO;
+                                                   }];
+                                                  
+                                                }];
+                             
                                          }];
                         
-                        // NSLog(@"LIKENESS OF TOP 4%d\n%d\n%d\n%d\n%d",[topFiveHanchos[0]likeness], [topFiveHanchos[1]likeness], [topFiveHanchos[2]likeness], [topFiveHanchos[3]likeness], [topFiveHanchos[4]likeness]);
-                    }];
-               }];
-          }];
-     }];
+                                               // NSLog(@"LIKENESS OF TOP 4%d\n%d\n%d\n%d\n%d",[topFiveHanchos[0]likeness], [topFiveHanchos[1]likeness], [topFiveHanchos[2]likeness], [topFiveHanchos[3]likeness], [topFiveHanchos[4]likeness]);
+                        }];
+                }];
+            }];
+        }];
 }
 
 - (void)didReceiveMemoryWarning
@@ -200,3 +229,4 @@
 }
 
 @end
+
