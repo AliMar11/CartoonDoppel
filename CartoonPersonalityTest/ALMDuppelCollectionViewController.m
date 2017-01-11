3128d7a44f7d4e1d77c1ba8076ae91f23ce36535
AliMar11 <alicia.marisal@gmail.com>
2017-01-10 23:55:00 -0500

    trial for reversing top collectionView in ALMDoppelCollectionViewController

--- CartoonPersonalityTest/ALMDuppelCollectionViewController.m
+++ CartoonPersonalityTest/ALMDuppelCollectionViewController.m
@@ -14,20 +14,29 @@
 @property (weak, nonatomic) IBOutlet UICollectionView *collectionViewOne;
 @property (nonatomic, strong) NSArray *doppelPictures;
 @property (nonatomic, assign) int horizontalScrollValue;
+@property (strong, nonatomic) ALMDuppelCollectionViewCell *cellOne;
+@property (strong, nonatomic) ALMDuppelCollectionViewCell *cellTwo;
+
+@property (strong, nonatomic) NSArray *dopplePicturesReversed;
 
 @end
 
 @implementation ALMDuppelCollectionViewController
 
-static NSString * const reuseIdentifier = @"dupplePictureCell";
+static NSString * const reuseIdentifier = @"doppelPictureCell";
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
-    [self.collectionViewOne registerClass: [ALMDuppelCollectionViewCell class] forCellWithReuseIdentifier: reuseIdentifier];
+    [self.collectionViewOne registerClass: [ALMDuppelCollectionViewCell class] forCellWithReuseIdentifier: @"doppelPictureCell"];
+    
+        [self.collectionViewOne registerClass: [ALMDuppelCollectionViewCell class] forCellWithReuseIdentifier: @"cellTwo"];
     
     self.doppelPictures = @[@"bart", @"flowey2", @"bob", @"buggs", @"louise", @"daffy5", @"frisk2", @"homer2", @"sam", @"sans3", @"tina3"];
+    
+    self.dopplePicturesReversed = @[@"tina3", @"sans3", @"sam", @"homer2", @"frisk2", @"daffy5", @"louise", @"buggs", @"bob", @"flowey2", @"bart"];
+    
 }
 
 //in viewDidAppear is when the collectionView scroll animation should happen, NSTimer creates smooth scrolling animation, NSRunLoop processes the timer, contentOffset is the actual scrolling motion.
@@ -36,12 +45,6 @@ static NSString * const reuseIdentifier = @"dupplePictureCell";
 {
     [super viewDidAppear: animated];
     animated = NO;
-    
-//    self.collectionViewOne.maskView.backgroundColor = [UIColor clearColor];
-//    self.collectionViewOne.backgroundView.backgroundColor = [UIColor clearColor];
-//    self.collectionViewOne.backgroundView.backgroundColor = [UIColor clearColor];
-//    self.collectionViewOne.backgroundView.backgroundColor = [UIColor clearColor];
-//    self.collectionViewOne.viewForLastBaselineLayout.backgroundColor = [UIColor clearColor];
     
     self.view.tintColor = [UIColor lightGrayColor];
     self.view.backgroundColor= [UIColor clearColor];
@@ -55,10 +58,19 @@ static NSString * const reuseIdentifier = @"dupplePictureCell";
     [[NSRunLoop currentRunLoop ]addTimer: timer forMode: NSRunLoopCommonModes];
     
 }
+
 -(void)scroll
 {
-    self.horizontalScrollValue += 2.5;
-    self.collectionViewOne.contentOffset = CGPointMake(self.horizontalScrollValue, 0);
+    if (self.collectionViewOne.tag == 1)
+    {
+        self.horizontalScrollValue -= 2.5;
+        self.collectionViewOne.contentOffset = CGPointMake(self.horizontalScrollValue, 0);
+    }
+    else
+    {
+        self.horizontalScrollValue += 2.5;
+        self.collectionViewOne.contentOffset = CGPointMake(self.horizontalScrollValue, 0);
+    }
 }
 
 #pragma mark <UICollectionViewDataSource>
@@ -76,30 +88,56 @@ static NSString * const reuseIdentifier = @"dupplePictureCell";
 
 - (UICollectionViewCell *)collectionView: (UICollectionView *)collectionView cellForItemAtIndexPath: (NSIndexPath *)indexPath
 {
-    ALMDuppelCollectionViewCell *cellOne = (ALMDuppelCollectionViewCell *)[collectionView dequeueReusableCellWithReuseIdentifier: reuseIdentifier forIndexPath: indexPath];
-
+    self.cellOne = (ALMDuppelCollectionViewCell *)[collectionView dequeueReusableCellWithReuseIdentifier: @"doppelPictureCell" forIndexPath: indexPath];
+    
+    self.cellTwo = (ALMDuppelCollectionViewCell *)[collectionView dequeueReusableCellWithReuseIdentifier: @"cellTwo" forIndexPath: indexPath];
         UIImage *mugshot = [[UIImage alloc] init];
 
-        if (cellOne)
+    if (self.cellTwo)
+    {
+        NSInteger nextImageIndex = indexPath.row % self.dopplePicturesReversed.count;
+        NSString *test = self.doppelPictures[nextImageIndex];
+        NSLog(@"current index: %lu, next index: %@", nextImageIndex,test);
+        
+        mugshot = [UIImage imageNamed: test];
+        UIImageView *mugshotView = [[UIImageView alloc] initWithImage: mugshot];
+        
+        mugshotView.autoresizingMask = NO;
+        mugshotView.autoresizesSubviews = NO;
+        mugshotView.translatesAutoresizingMaskIntoConstraints = NO;
+        mugshotView.frame = self.cellTwo.bounds;
+        mugshotView.contentMode = UIViewContentModeScaleToFill;
+        mugshotView.clipsToBounds = YES;
+        mugshotView.layer.cornerRadius = self.cellTwo.frame.size.height/2;
+        
+        [self.cellTwo addSubview: mugshotView];
+        
+        return  self.cellTwo;
+   
+    }
+        else if (self.cellOne)
         {
             NSInteger otherIndex = indexPath.row % self.doppelPictures.count;
             NSString *test = self.doppelPictures[otherIndex];
             
+            NSLog(@"no tag index: %@", test);
             mugshot = [UIImage imageNamed: test];
             UIImageView *mugshotView = [[UIImageView alloc] initWithImage: mugshot];
             
             mugshotView.autoresizingMask = NO;
             mugshotView.autoresizesSubviews = NO;
             mugshotView.translatesAutoresizingMaskIntoConstraints = NO;
-            mugshotView.frame = cellOne.bounds;
+            mugshotView.frame = self.cellOne.bounds;
             mugshotView.contentMode = UIViewContentModeScaleToFill;
             mugshotView.clipsToBounds = YES;
-            mugshotView.layer.cornerRadius = cellOne.frame.size.height/2;
+            mugshotView.layer.cornerRadius = self.cellOne.frame.size.height/2;
     
-            [cellOne addSubview: mugshotView];
+            [self.cellOne addSubview: mugshotView];
+            
+            return self.cellOne;
         }
-
-    return cellOne;
+    return self.cellOne;
+    
 }
 
 #pragma mark <collectionView visualSetup>
