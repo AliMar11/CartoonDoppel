f0e25f220e8cdb67c7617bdddfd647f50f279b99
AliMar11 <alicia.marisal@gmail.com>
2017-01-17 12:27:27 -0500

    added share VC

---
+++ CartoonPersonalityTest/ALMShareViewController.h
@@ -0,0 +1,13 @@
+//
+//  ALMShareViewController.h
+//  CartoonPersonalityTest
+//
+//  Created by Alicia Marisal on 1/15/17.
+//  Copyright © 2017 AliApps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALMShareViewController : UIViewController
+
+@end
---
+++ CartoonPersonalityTest/ALMShareViewController.m
@@ -0,0 +1,39 @@
+//
+//  ALMShareViewController.m
+//  CartoonPersonalityTest
+//
+//  Created by Alicia Marisal on 1/15/17.
+//  Copyright © 2017 AliApps. All rights reserved.
+//
+
+#import "ALMShareViewController.h"
+#import "ALMButtonLayer.h"
+#import "ALMBackgroundLayer.h"
+
+@interface ALMShareViewController ()
+
+@end
+
+@implementation ALMShareViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
