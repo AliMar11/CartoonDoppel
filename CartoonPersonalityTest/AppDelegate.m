e4bc3d3cdcdae8b6d3a472eb1dddc76995cc9c83
AliMar11 <alicia.marisal@gmail.com>
2017-02-13 23:50:51 -0500

    added LaunchScreen icon sizes

--- CartoonPersonalityTest/AppDelegate.m
+++ CartoonPersonalityTest/AppDelegate.m
@@ -10,6 +10,7 @@
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <Fabric/Fabric.h>
 #import <TwitterKit/TwitterKit.h>
+#import "ALMStartScreenViewController.h"
 
 
 @interface AppDelegate ()
@@ -21,6 +22,19 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+         UIImageView *launchImage = [[UIImageView alloc] initWithImage: [UIImage imageNamed: @"LaunchScreen"]];
+          ALMStartScreenViewController *startScreen = [[ALMStartScreenViewController alloc] init];
+        
+         //UIImageView*imageView=[[UIImageView alloc]initWithImage:[UIImage imageNamed:@"LaunchImage"]];
+         [[startScreen view] addSubview: launchImage];
+         [[startScreen view] bringSubviewToFront: launchImage];
+        
+       //  as usual
+          [self.window makeKeyAndVisible];
+        
+       // now fade out splash image
+        [UIView transitionWithView:self.window duration:2.0f options:UIViewAnimationOptionTransitionNone animations:^(void){launchImage.alpha=0.0f;} completion:^(BOOL finished){[launchImage removeFromSuperview];}];
+        
     [Fabric with:@[[Twitter class]]];
 
     return YES;
